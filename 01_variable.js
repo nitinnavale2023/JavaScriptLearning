9719c6c33cbf957337b4ea828727b3b1f11b3ab5
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-07 20:13:44 -0800

    variable declaration, initiliization and updated is used
    variable declaration, initiliization and updated is used

---
+++ 01_variable.js
@@ -0,0 +1,7 @@
+var myName;  //Variable Declaretion
+myName = "Nitin Navale";  //Variable Initialization
+console.log(myName);
+var myFrdName = "Akshay Dounde";  //Variable Declaration and Initilization
+console.log(myFrdName);
+myFrdName = "Onkar Navale";  //Variable Updated
+console.log(myFrdName);
---
+++ 01_variable_assign.js
@@ -0,0 +1,25 @@
+var myFullName;
+console.log(myFullName);
+myFullName = "Nitin Navale";
+console.log(myFullName);
+var myFullName = "Nitin Vilas Navale";
+console.log(myFullName);
+var myLove = "I Love Only JS";
+console.log( myLove);
+var firstName;
+var lastName;
+var age;
+var adderess;
+var pin_cod;
+var gender;
+var is_married;
+firstName = "Nitin";
+lastName = "Navale";
+age = "28";
+adderess = "sangola";
+pin_cod = "413307";
+gender =  "male";
+is_married = "married";
+is_married = "married";
+console.log(firstName,lastName,age,adderess,pin_cod,gender,);
+

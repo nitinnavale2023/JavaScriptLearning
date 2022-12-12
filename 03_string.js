a41ea537ab5211bf28da3115e579f10e2ba1d7fa
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-12 03:56:45 -0800

    String index in js
    String index in js

---
+++ 03_string.js
@@ -0,0 +1,7 @@
+var firstName = "Nitin Navale";{ 
+console.log("Given String", firstName);
+console.log("Total character in the String :", firstName.length);
+console.log("Char at index 3 :", firstName.charAt(3));
+var lengthTotal = firstName.length;
+console.log("Char at last index :", firstName.charAt(lengthTotal-1));
+}

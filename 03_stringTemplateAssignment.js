d27e5b25753d23cbcc96e337c67e557b1179b621
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-13 05:53:55 -0800

    string tmplate Assignment
    string tmplate Assignment

---
+++ 03_stringTemplateAssignment.js
@@ -0,0 +1,9 @@
+console.log(`My Dream Company is "Infosys"`);
+console.log("---------------------------------------------------------------------------------------");
+var myHobbies1 = "PlayingCricket";
+var myHobbies2 =  "WatchingMovie";
+var myHobbies3 = "SolveQueries";
+console.log(`my Hobbies are :-${myHobbies1},  ${myHobbies2},  ${myHobbies3}`);
+console.log("---------------------------------------------------------------------------------------");
+var result = myHobbies1 + myHobbies2 + myHobbies3;
+console.log(result);

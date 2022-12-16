b12d5779a9ebaa6220b23608a7775c8fd4a1382b
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-16 09:09:25 -0800

    chaking intresting fact in javaScript
    chaking intresting fact in javaScript

---
+++ assignment_0C.js
@@ -0,0 +1,40 @@
+
+
+var result = 0=='';
+console.log(`Result of 0==' ' :- ${result}`);
+
+var res = 0=='0';
+console.log(`Result of 0=='0' :- ${res}`);
+
+var trueFalse = 0==false;
+console.log(`Result of 0==false :- ${trueFalse}`);
+
+ var nul = null==undefined;
+ console.log(`Result of null==undefined :- ${nul}`);
+
+ var nik = 1==[1];
+ console.log(`Result of 1==[1] :- ${nik}`);
+
+ var val = 1==true;
+ console.log(`Result of 1==true :- ${val}`);
+
+ var equ = 1=='1';
+ console.log(`Result of 1=='1' :- ${equ}`);
+
+console.log("----------------------------------------------------------------------------------------------------------------")
+
+ var nu = 4+undefined;
+ console.log("Result of 4 + undefined is :-",nu);
+
+ var value = '3'+2;
+ console.log("Result of '3'+ 2 is :-",value);
+
+ var va = '4'-true;
+ console.log("Result of '4'- true is :-",va);
+
+ var na = 4+true;
+ console.log("Result of 4 + true is :-",na);
+
+ var me = '3'+null;
+ console.log("Result of '3'+ nul is :-",me);
+

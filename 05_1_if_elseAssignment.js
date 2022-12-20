42e6bd5473c517b0e85114c71a2aca9696f05ac9
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-20 09:39:11 -0800

    voting case and mark in grade complite code in js if else  setement
    voting case and mark in grade complite code in js if else  setement

---
+++ 05_1_if_elseAssignment.js
@@ -0,0 +1,64 @@
+
+
+ function voting(age){
+    if(age<=0 || age>120){
+        console.log(`Invalid data`);
+    }
+    else 
+    if(age>=18){
+        console.log(`voting age ${age} for eligible`);
+    }
+  else{
+    console.log(`voting age ${age} for Not eligible`);
+  }
+}
+voting(45);
+voting(17);
+voting(8);
+voting(20);
+voting(-10);
+voting(200);
+voting(0);
+
+console.log(`------------------------------------------------------------------------------------------------------------------------`);
+
+
+function gradeCalculation(marks){
+   var a = typeof marks 
+    else
+    if(marks<=0 ||marks>100)
+    {
+        console.log(`Please Provide the valid Marks`);
+    }
+    if(marks>=90)
+    {
+        console.log(`Funtastic Marks :- ${marks}, Your grade is A+`);
+    }
+     if(marks>=75 && marks<90)
+    {
+        console.log(`Excellent Marks :- ${marks}, Your grade is A`);
+    }
+     if(marks>=50 && marks<75)
+    {
+        console.log(`Good Marks :- ${marks}, Your grade is B`);
+    }
+    if(marks>=35 && marks<50)
+    {
+        console.log(` Marks is :- ${marks}, Your grade is C, Need Improvement`);
+    }
+    if( marks<35){
+        console.log(` Marks is ${marks}, You are Fail`);
+    }
+   }
+gradeCalculation(98);
+gradeCalculation(80);
+gradeCalculation(90);
+gradeCalculation(0);
+gradeCalculation(150);
+gradeCalculation(-7);
+gradeCalculation(35);
+gradeCalculation(29);
+gradeCalculation(64);
+gradeCalculation(49);
+gradeCalculation("91");
+gradeCalculation("Eighty");

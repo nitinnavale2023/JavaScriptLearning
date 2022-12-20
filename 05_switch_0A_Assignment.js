f124a68c90e990bf0bda9c755b3d917f43bccce4
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-20 09:43:59 -0800

    Switch Statement Assignment  in java Script
    Switch Statement Assignment  in java Script

---
+++ 05_switch_0A_Assignment.js
@@ -0,0 +1,54 @@
+function monthOfYear(monthNumber) {
+  switch (monthNumber) {
+    case 1:
+      console.log(`Januaru`);
+      break;
+    case 2:
+      console.log(`Feburuary`);
+      break;
+    case 3:
+      console.log(`March`);
+      break;
+    case 4:
+      console.log(`Afril`);
+      break;
+      case 5: console.log(`May`);
+      break;
+      case 6: console.log(`Jun`);
+      break;
+    case 7:
+      console.log(`Jule`);
+      break;
+    case 8:
+      console.log(`Agust`);
+      break;
+      case 9: console.log(`September`);
+      break;
+    case 10:
+      console.log(`Octomber`);
+      break;
+    case 11:
+      console.log(`November`);
+      break;
+    case 12:
+      console.log(`December`);
+      break;
+    default:
+      console.log(`invalid data`);
+
+      break;
+  }
+}
+monthOfYear(1);
+monthOfYear(2);
+monthOfYear(3);
+monthOfYear(4);
+monthOfYear(5);
+monthOfYear(6);
+monthOfYear(7);
+monthOfYear(8);
+monthOfYear(9);
+monthOfYear(10);
+monthOfYear(11);
+monthOfYear(12);
+monthOfYear(15);

757c51b4b181c5a3057eff07483302023852e9a4
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-22 10:23:03 -0500

    Braek and Continue Stetment usin in js
    Braek and Continue Stetment usin in js

---
+++ 05_braekAndContinue.js
@@ -0,0 +1,37 @@
+
+
+// var index = 0;
+// while(index<=10){
+//     console.log(index);
+//     if(index==5){
+//         break;
+//     }
+//     index++;
+// }
+// console.log(`-------------------------------------------------------------------------`);
+// for (let index = 0; index < 20; index++) {
+//     console.log(index);
+//     if (index>=12) {
+//       break;
+//     }
+     
+//  }
+ console.log(`-------------------------------------------------------------------------`);
+
+//  var index = 0;
+// while(index<=10){
+//     console.log(index);
+//     if(index==5){
+//        continue;
+//     }
+//     index++;
+// }
+console.log(`-------------------------------------------------------------------------`);
+
+for (let index = 0; index < 20; index++) {
+    console.log(index);
+    if (index==12) {
+      continue;
+    }
+     
+ }

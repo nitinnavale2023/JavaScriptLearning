42544531784e48cd349594f0fdb14098060c6436
nitinnavale2023 <nitinnavale2023@gmail.com>
2022-12-30 00:04:57 -0500

    Object Assignment complited
    Object Assignment complited

---
+++ 07_objectAssignment_2.js
@@ -0,0 +1,40 @@
+const sbiBank ={
+    bankName: "SBI Bank",
+    location: "pune",
+    accountNo: "0060001010000198",
+    ifsc : "SBIN00001241",
+    interestRate: "5%",
+
+}
+const axisBank ={
+    bankName: "Axis Bank",
+    location: "Solapur",
+    accountNo: "0060001010000552",
+    ifsc : "AXSIN00001277",
+    interestRate: "7%",
+}
+const hdfcBank ={
+    bankName: "HDFC Bank",
+    location: "kolapur",
+    accountNo: "0010001510000851",
+    ifsc : "HDFc00001277",
+    interestRate: "9%",
+}
+const urbanBank ={
+    bankName: "Urban Bank",
+    location: "Sangola",
+    accountNo: "0060001010000166",
+    ifsc : "URBN00001593",
+    interestRate: "6%",
+}
+console.log(`1)=========SBI Bank================`);
+console.log(sbiBank);
+
+console.log(`2)=========Axis Bank================`);
+console.log(axisBank);
+
+console.log(`3)=========HDFC Bank================`);
+console.log(hdfcBank);
+
+console.log(`4)==========Urban Bank================`);
+console.log(urbanBank);

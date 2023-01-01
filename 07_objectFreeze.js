1c7d7b75f75c39e80d5f72f8b2befde5af5c0e77
nitinnavale2023 <nitinnavale2023@gmail.com>
2023-01-01 12:28:48 -0500

    basic Object freezee
    basic Object freezee

---
+++ 07_objectFreeze.js
@@ -0,0 +1,34 @@
+const anil = {
+    age:23,
+    name : "Anil",
+    company : "Microsoft"
+}
+Object.freeze(anil);
+anil.country = "India";
+anil.age = 27;
+delete anil.company;
+console.log(anil);
+
+// Array freezing 
+const arrayNumbers = [2, 4, 5, 8];
+// arrayNumbers = []; /// Not allowed
+// Freeze array
+Object.freeze(arrayNumbers);
+//arrayNumbers.push(10); // Not allowed
+console.log(arrayNumbers);
+
+
+const sunil ={
+    age: 24,
+    name: "sunil",
+    company: "Apple",
+}
+const sunilAddress = {
+    city : "Pune",
+    pin: "431202"
+}
+
+// Merging two objects
+Object.assign(sunil,sunilAddress);
+console.log(sunil);
+console.log(sunilAddress);

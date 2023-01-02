b5350880bb797c8828dcb13798f0b033881ccc08
nitinnavale2023 <nitinnavale2023@gmail.com>
2023-01-02 02:53:02 -0500

    Basic Array Cloning in js
    Basic Array Cloning in js

---
+++ 07_arreCloning.js
@@ -0,0 +1,15 @@
+console.log("Array shallow Cloning");
+let arrayOfEvenNum = [0, 2, 4, 6, 8 ];
+let arrayOfNum = arrayOfEvenNum; // Shallow Cloning
+arrayOfEvenNum[1] = 10;
+console.log(arrayOfEvenNum);
+console.log(arrayOfNum);
+
+// Deep Cloning
+console.log("Array Deep Cloning");
+let arrayOfNumber = [0, 2, 4, 6, 8 ];
+let arrayClone =  [... arrayOfNumber]; // Deep Clone
+arrayOfNumber[2] = 100;
+arrayClone[2] = 200;
+console.log(arrayOfNumber);
+console.log(arrayClone);
